9671a6103ecee9b58caf6d1d11b9b67369822331
SerovaKaterina <k.serova.k@yandex.ru>
2023-06-12 20:46:09 +0300

    add changes

--- part2/index2.js
+++ part2/index2.js
@@ -3,3 +3,9 @@ const getLastWordLength = (str) => {
     let lastWord = allWords.at(-1);
     return lastWord.length;
 };
+
+
+/* Условия выполняются
+getLastWordLength(''); // 0
+getLastWordLength('man in BlacK'); // 5
+getLastWordLength('hello, world!  '); // 6*/
